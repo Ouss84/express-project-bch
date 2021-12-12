f315cbf7ee3f5a62bf90f500ac9cf14c57455908
Ouss84 <bahrioussama84@gmail.com>
2021-12-12 18:50:29 +0200

    adding the getOneItem page display and functionality

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const DataStorage = require(path.join(
   storage.storageFolder,
   storage.dataLayer
 ));
+const menuPath = path.join(__dirname, "landingPage.html");
 
 const storedData = new DataStorage();
 const server = http.createServer(app);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "htmlPages"));
-const menuPath = path.join(__dirname, "landingPage.html");
 
 app.get("/", (req, res) => res.sendFile(menuPath));
 
@@ -26,4 +26,30 @@ app.get("/all", (req, res) =>
   storedData.getAll().then((data) => res.render("inventory", { res: data }))
 );
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/getComputer", (req, res) =>
+  res.render("getOneItem", {
+    title: "Get one item",
+    header: "Get one item",
+    action: "/getComputer",
+  })
+);
+app.post("/getComputer", (req, res) => {
+  if (!req.body) res.sendStatus(500);
+  const itemId = req.body.id;
+  storedData
+    .getOne(itemId)
+    .then((item) => res.render("itemDisplay", { res: item }))
+    .catch((error) => sendErrorPage(res, error));
+});
+
+// Status Pages
+function sendErrorPage(res, error, title = "Error", header = "Error") {
+  sendStatusPage(res, error, title, header);
+}
+function sendStatusPage(res, status, title = "status", header = "status") {
+  return res.render("statusPage", { title, header, status });
+}
 server.listen(port, host, () => console.log(`${host}:${port} serving...`));
